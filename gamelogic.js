04b656e116644093465c8f3cd5c37f81ea28c79c
Luigi Mario <167142658+trashmotel@users.noreply.github.com>
2024-11-11 18:58:45 -0500

    Add comments for first function

--- gamelogic.js
+++ gamelogic.js
@@ -1,3 +1,5 @@
 function getComputerChoice() {
-    
+    //use If Else statements to return rock, paper, or scissors
+    //use Math.random to generate a number which will determine the string returned
+
 }
