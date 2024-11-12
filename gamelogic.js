71fe00f0e0c03fb4d2fd30934540179661826908
Luigi Mario <167142658+trashmotel@users.noreply.github.com>
2024-11-11 20:01:34 -0500

    Add statements to complete function.

--- gamelogic.js
+++ gamelogic.js
@@ -1,5 +1,13 @@
 function getComputerChoice() {
+    let x = Math.random();
     //use If Else statements to return rock, paper, or scissors
+    if (x < 0.4) {
+        return 'rock';
+    } else if (x < 0.7) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
     //use Math.random to generate a number which will determine the string returned
 
 }
